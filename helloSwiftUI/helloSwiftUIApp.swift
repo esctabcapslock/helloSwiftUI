a85f84c604a90770ce747e5711774d7ab16be7c4
esctabcapslock <62400002esctabcapslock@users.noreply.github.com>
2023-02-18 16:51:40 +0100

    Initial Commit

---
+++ helloSwiftUI/helloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloSwiftUIApp.swift
+//  helloSwiftUI
+//
+//  Created by zffridption on 2023. 2. 18..
+//
+
+import SwiftUI
+
+@main
+struct helloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
